b037ea85c4b576cfccbc5e9ef6bbf975be2ca447
lzy <fuzzyy@foxmail.com>
2019-12-04 10:55:02 +0800

    修改资源文件

--- source/js/background.js
+++ source/js/background.js
@@ -1,30 +1,10 @@
-﻿chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-    chrome.tabs.sendMessage(tabId, {greeting: "hello"}, function (response) {
-        // console.log(response, 222222222222222);　　// 向content-script.js发送请求信息
-        // loadXMLDoc('https://m.baidu.com');
-        $.get("https://item.m.jd.com/product/7651951.html", function (data, status) {
-            console.log("Data: " + data + "nStatus: " + status, 333333);
-        });
+﻿// 监听来自content-script的消息
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log('收到来自content-script的消息：');
+    console.log(request, sender, sendResponse);
+    $.ajaxSettings.async = false;//同步发送请求
+    $.get("https://item.jd.com/62496752295.html", function (data, status) {
+        console.log(data);
+        sendResponse(data);
     });
 });
-
-function loadXMLDoc(url) {
-    xmlhttp = new XMLHttpRequest();
-    if (xmlhttp != null) {
-        xmlhttp.onreadystatechange = state_Change;
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send(null);
-    } else {
-        console.log("Your browser does not support XMLHTTP.");
-    }
-}
-
-function state_Change() {
-    if (xmlhttp.readyState == 4) {// 4 = "loaded"
-        if (xmlhttp.status == 200) {// 200 = "OK"
-            console.log(xmlhttp.responseText);
-        } else {
-            console.log("Problem retrieving data:" + xmlhttp.statusText);
-        }
-    }
-}
--- source/js/contentscript.js
+++ source/js/contentscript.js
@@ -1,10 +1,9 @@
-var html = document.body.innerHTML;
-chrome.extension.onMessage.addListener(
-    function (request, sender, sendMessage) {
-        if (request.greeting == "hello") {
-            console.log(html);
-            sendMessage(html);
-        } else {
-            sendMessage("FUCK OFF");// snub them.
-        }
+//注入的JS
+window.abc = 1;
+document.querySelector('body').addEventListener('click', function () {
+    console.log('你点击了页面', 6666666666666);
+    chrome.runtime.sendMessage({greeting: '你好，我是content-script呀，我主动发消息给后台！'}, function (response) {
+        console.log('收到来自后台的回复：' + response);
+        document.getElementById('kw').value = response;
     });
+});
