cef9ace78104259941329246f7d7498beed0072b
lzy <fuzzyy@foxmail.com>
2019-12-05 09:57:37 +0800

    完善注入的JS

--- source/js/contentscript.js
+++ source/js/contentscript.js
@@ -1,23 +1,28 @@
 //注入的JS
-$(document).on("click", ".btn-goods-detail", function () {
-    var fetchurl = document.getElementById("fetchurl").value;
-    if (!fetchurl) {
-        console.log("url不能为空");
-    }
-    chrome.runtime.sendMessage(
-        {
-            action: 'fetchUrl',
-            url: fetchurl
+var goodsDetailSelector = $(".btn-goods-detail");
+if (goodsDetailSelector) {
+    goodsDetailSelector.removeClass('hide');
+    $("#downloadcrx").addClass('hide');
+    $(document).on("click", ".btn-goods-detail", function () {
+        var fetchurl = document.getElementById("fetchurl").value;
+        if (!fetchurl) {
+            console.log("url不能为空");
         }
-        , function (response) {
-            if (response.code == 1) {
-                if (typeof response.result == 'string') {
-                    document.getElementById("goodshtml").value = response.result;
-                } else {
-                    document.getElementById("goodshtml").value = JSON.stringify(response.result);
-                }
-            } else {
-                console.log(response.result);
+        chrome.runtime.sendMessage(
+            {
+                action: 'fetchUrl',
+                url: fetchurl
             }
-        });
-});
+            , function (response) {
+                if (response.code == 1) {
+                    if (typeof response.result == 'string') {
+                        document.getElementById("goodshtml").value = response.result;
+                    } else {
+                        document.getElementById("goodshtml").value = JSON.stringify(response.result);
+                    }
+                } else {
+                    console.log(response.result);
+                }
+            });
+    });
+}
